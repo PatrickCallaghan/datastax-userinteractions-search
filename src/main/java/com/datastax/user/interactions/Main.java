1b5fe6bf4f8d7e85dfce9c4d069e82c3b46a4823
Patrick Callaghan <patcho@gmail.com>
2015-11-26 14:13:03 +0000

    first commit

---
+++ src/main/java/com/datastax/user/interactions/Main.java
@@ -0,0 +1,135 @@
+package com.datastax.user.interactions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import com.datastax.user.interactions.dao.UserRepository;
+import com.datastax.user.interactions.model.UserInteraction;
+
+public class Main {
+
+	private static final int USER_VISITS = 1000000;
+	private static final int NO_OF_USERS = 100000;
+
+	private UserRepository repository;
+	private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+	private DateTime dateTime = new DateTime().minusDays(20);
+	private Map<String, Date> userDateMap = new HashMap<String, Date>();
+	
+	public static void main(String args[]) {
+		new Main();
+    }
+    
+    public Main(){
+    	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new ClassPathResource("spring-context.xml").getPath());
+    	repository = context.getBean(UserRepository.class);
+    	
+    	for (int i = 0; i < USER_VISITS; i++){
+    		
+    		//Creates from a pool of 1000
+    		repository.save(createRandomUserInteraction(NO_OF_USERS));
+    		
+    		if (i%10000 ==0){
+    			logger.info("Processed " + i + " users visits.");
+    		}
+    	}
+	}
+
+	private List<UserInteraction> createRandomUserInteraction(int noOfUsers) {
+
+		List<UserInteraction> interactions = new ArrayList<UserInteraction>();
+
+		UUID id = UUID.randomUUID();
+		String user = "U" + (new Double(Math.random() * noOfUsers).intValue() + 1);
+		String app = apps.get(new Double(Math.random() * apps.size()).intValue());
+		String userAgent = userAgents.get(new Double(Math.random() * userAgents.size()).intValue());
+
+		// Need to establish a good date. now or after the user logged out of
+		// the last app they were using
+		DateTime dateTime = new DateTime();
+
+		if (userDateMap.containsKey(user)) {
+			dateTime = new DateTime(userDateMap.get(user));
+			dateTime = dateTime.plusSeconds(new Double(Math.random() * 60).intValue());
+		} else {
+			dateTime = new DateTime();
+		}
+
+		UUID correlationId = UUID.randomUUID();
+
+		UserInteraction interaction = new UserInteraction();
+		interaction.setClientid(app);
+		interaction.setCorrelationid(correlationId.toString());
+		interaction.setDateTime(dateTime.toDate());
+		interaction.setDetails("Login");
+		interaction.setEvent_type("Login");
+		interaction.setId(UUID.randomUUID());
+		interaction.setReference("");
+		interaction.setUser_agent(userAgent);
+		interaction.setUserid(user);
+
+		interactions.add(interaction);
+
+		int items = new Double(Math.random() * 50).intValue() + 1;
+
+		for (int i = 0; i < items; i++) {
+
+			dateTime = dateTime.plusSeconds(new Double(Math.random() * 60).intValue());
+
+			String action = actions.get(new Double(Math.random() * actions.size()).intValue());
+
+			interaction = new UserInteraction();
+			interaction.setClientid(app);
+			interaction.setCorrelationid(correlationId.toString());
+			interaction.setDateTime(dateTime.toDate());
+			interaction.setDetails(action);
+			interaction.setEvent_type(action);
+			interaction.setId(UUID.randomUUID());
+			interaction.setReference("");
+			interaction.setUser_agent(userAgent);
+			interaction.setUserid(user);
+
+			interactions.add(interaction);
+		}
+
+		interaction = new UserInteraction();
+		interaction.setClientid(app);
+		interaction.setCorrelationid(correlationId.toString());
+		interaction.setDateTime(dateTime.toDate());
+		interaction.setDetails("Logout");
+		interaction.setEvent_type("Logout");
+		interaction.setId(UUID.randomUUID());
+		interaction.setReference("");
+		interaction.setUser_agent(userAgent);
+		interaction.setUserid(user);
+
+		interactions.add(interaction);
+		userDateMap.put(user, dateTime.toDate());
+
+		return interactions;
+	}
+
+	private List<String> userAgents = Arrays.asList(
+			"Some User Agents for OS - WindowsInternet Explorer 7 (Windows Vista)", "Safari 125.8 (MacintoshOSX)",
+			"Mozilla 1.6 (Debian)", "Googlebot 2.1 (New version)", "FireFox 2.0.0.19 (Ubuntu)",
+			"Google Chrome 0.2.149.29 (Windows XP)", "Firefox 0.9 (MacintoshOSX )");
+
+	private List<String> apps = Arrays.asList("mobilefx", "mobilefx", "mobilefx", "desktop ui", "tablet research",
+			"tablet research", "iphone banking", "android banking", "mobile trading");
+
+	private List<String> actions = Arrays.asList("tradeview", "news", "portfolio", "p and l", "charting",
+			"advanced charting", "advanced charting", "advanced charting", "news", "news", "landing page",
+			"preferences", "tradeview", "tradeview", "tradeview", "tradeview", "tradeview");
+}
